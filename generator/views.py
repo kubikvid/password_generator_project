ac5b3e686751d1811c84c7a444795c8c6ac94a9c
Temirlan <tbulebaev@bk.ru>
2022-04-29 22:01:42 +0300

    do special

--- generator/views.py
+++ generator/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 # Create your views here.
 
 def home(request):
     return render(request, 'generator/home.html')
 
 def password(request):
-    return render(request, 'generator/password.html')
+
+    characters = list('abcdefghijklmnopqrstuvwxyz') #создаем список символов из строки
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')) #добавляем к списку заглавные символы
+
+    if request.GET.get('special'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')) #добавляем к списку заглавные символы
+
+
+    length = int(request.GET.get('length', 12))# Получаем параметры из строки запроса
+    the_password = ''
+    for x in range(length):
+        the_password += random.choice(characters) #Выбират случайные буквы
+    return render(request, 'generator/password.html', {'password': the_password})
