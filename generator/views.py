81013cebe7d94d4a22d7a1ea44f5cad09f049d42
Temirlan <tbulebaev@bk.ru>
2022-04-30 16:25:51 +0300

    show

--- generator/views.py
+++ generator/views.py
@@ -14,11 +14,14 @@ def password(request):
         characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')) #добавляем к списку заглавные символы
 
     if request.GET.get('special'):
-        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')) #добавляем к списку заглавные символы
+        characters.extend(list('!@#$%^&*()-_+=;:,./?\|`~[]{}')) #добавляем к списку специальные символы
+
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789')) #добавляем к списку цифры
 
 
     length = int(request.GET.get('length', 12))# Получаем параметры из строки запроса
     the_password = ''
     for x in range(length):
-        the_password += random.choice(characters) #Выбират случайные буквы
+        the_password += random.choice(characters) #Выбират случайные символы
     return render(request, 'generator/password.html', {'password': the_password})
--- password_generator/urls.py
+++ password_generator/urls.py
@@ -17,6 +17,6 @@ from django.urls import path
 from generator import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
     path('generatedpassword/', views.password, name='password'),
 ]
